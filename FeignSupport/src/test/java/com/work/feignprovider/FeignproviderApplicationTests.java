d341a72ad4d03e3618795f3f817615c6268da890
loganabc <c1520416296@126.com>
2020-11-02 20:26:31 +0800

    add code

---
+++ FeignSupport/src/test/java/com/work/feignprovider/FeignproviderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.work.feignprovider;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeignproviderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
